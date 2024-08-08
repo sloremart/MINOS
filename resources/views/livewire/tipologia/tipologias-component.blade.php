36dc1a2d8c088189d69a00453ac6dbd99cedb668
gabriel <gaboandres1197@gmail.com>
2024-08-08 15:11:04 -0500

    +++

--- resources/views/livewire/tipologia/tipologias-component.blade.php
+++ resources/views/livewire/tipologia/tipologias-component.blade.php
@@ -12,28 +12,31 @@
                     <b>{{ $componetName }} | {{ $pageTitle }}</b>
 
                     <div>
-                        <div class="flex items-center  justify-between flex-column md:flex-row flex-wrap space-y-4 md:space-y-0 py-4 bg-white dark:bg-gray-900 rounded-3xl">
+                        <div
+                            class="flex items-center  justify-between flex-column md:flex-row flex-wrap space-y-4 md:space-y-0 py-4 bg-white dark:bg-gray-900 rounded-3xl">
                             <div>
                                 <div class="inline-flex items-center rounded-3xl ">
-                                    <button type="button" data-modal-target="tipologias" data-modal-toggle="tipologias" class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-full group bg-gradient-to-br from-purple-800 to-purple-800 group-hover:from-purple-800 group-hover:to-purple-800 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-800 dark:focus:ring-purple-800">
-                                        <span class="relative px-4 py-4 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-full group-hover:bg-opacity-0 text-purple-700 hover:text-white" style="font-size: 20px">
-                                            <i class="fa-regular fa-circle-plus"></i>
-                                        </span>
+                                    <button type="button" data-modal-target="tipologias" data-modal-toggle="tipologias"
+                                        class="text-white bg-gradient-to-r from-purple-700 via-purple-700 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-700 font-medium  hover:text-white   text-lg px-5 py-5 text-center me-2 mb-2 grid place-content-cente rounded-full p-0.5" >
+                                        <i class="fa-regular fa-circle-plus"></i>
                                     </button>
-                                    <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-full group bg-gradient-to-br from-red-800 to-red-800 group-hover:from-red-800 group-hover:to-red-800 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-red-800 dark:focus:ring-red-800">
-                                        <span class="relative px-4  py-4 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-full group-hover:bg-opacity-0 text-red-700 hover:text-white" style="font-size: 20px">
+                                    
+                                    <button
+                                    class="text-white bg-gradient-to-r from-red-800 via-red-800 to-red-800 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium  hover:text-white   text-lg px-5 py-5 text-center me-2 mb-2 grid place-content-cente rounded-full p-0.5" >
+                                        
                                             <i class="fa-solid fa-file-pdf"></i>
-                                        </span>
+                                       
                                     </button>
-                                    <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-full group bg-gradient-to-br from-green-600 to-green-700 group-hover:from-green-700 group-hover:to-green-700 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-700 dark:focus:ring-green-700">
-                                        <span class="relative px-4  py-4 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-full group-hover:bg-opacity-0 text-green-700 hover:text-white" style="font-size: 20px">
+                                    <button
+                                    class="text-white bg-gradient-to-r from-green-800 via-green-800 to-green-800 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium  hover:text-white   text-lg px-5 py-5 text-center me-2 mb-2 grid place-content-cente rounded-full p-0.5" >
+                                       
                                             <i class="fa-solid fa-file-excel"></i>
-                                        </span>
+                                       
                                     </button>
-                                    <button class="relative inline-flex   items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-full group bg-gradient-to-br from-gray-600 to-gray-500 group-hover:from-gray-600 group-hover:to-gray-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-                                        <span class="relative px-4  py-4 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-full group-hover:bg-opacity-0 text-gray-700 hover:text-white" style="font-size: 20px">
+                                    <button
+                                    class="text-white bg-gradient-to-r from-gray-800 via-gray-800 to-gray-800 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:gray-green-300 dark:focus:ring-green-800 font-medium  hover:text-white   text-lg px-5 py-5 text-center me-2 mb-2 grid place-content-cente rounded-full p-0.5" >
                                             <i class="fa-solid fa-print"></i>
-                                        </span>
+                                       
                                     </button>
 
 
@@ -42,20 +45,28 @@
                             </div>
                             <label for="table-search" class="sr-only">Search</label>
                             <div class="relative">
-                                <div class="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
-                                    <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                                <div
+                                    class="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
+                                    <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                            stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                                     </svg>
                                 </div>
-                                <input type="text" id="table-search-users" wire:model.live="buscar" class="block pt-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search for users">
+                                <input type="text" id="table-search-users" wire:model.live="buscar"
+                                    class="block pt-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    placeholder="Search for users">
                             </div>
                         </div>
                     </div>
 
-                    <div class="w-full text-sm text-left rtl:text-right bg-gray-100 text-gray-600 dark:text-gray-400 rounded-3xl overflow-hidden border-2  border-gray-500">
+                    <div
+                        class="w-full text-sm text-left rtl:text-right bg-gray-100 text-gray-600 dark:text-gray-400 rounded-3xl overflow-hidden border-2  border-gray-500">
                         <div class="overflow-x-auto custom-scrollbar ">
-                            <table class="min-w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 ">
-                                <thead class="text-xs text-gray-200 h-16 uppercase bg-gray-500 dark:bg-gray-700 dark:text-gray-400">
+                            <table
+                                class="min-w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 ">
+                                <thead
+                                    class="text-xs text-gray-200 h-16 uppercase bg-gray-500 dark:bg-gray-700 dark:text-gray-400">
                                     <tr>
                                         <th scope="col" class="px-6 py-3">#</th>
                                         <th scope="col" class="px-6 py-3">Nombre Unidad</th>
@@ -66,26 +77,34 @@
                                 </thead>
                                 <tbody>
                                     @foreach ($data as $tipologia)
-                                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                        <td class="px-6 py-4">{{ $tipologia->id }}</td>
-                                        <td class="px-6 py-4 text-center">{{ $tipologia->nombre_uni }}</td>
-                                        <td class="px-6 py-4 text-center">{{ $tipologia->abreviatura }}</td>
-                                        <td class="px-6 py-4 text-center">{{ $tipologia->estatus }}</td>
-                                        <td class="text-end">
-                                            <div class="btn-group" role="group" aria-label="Basic example">
-                                                <button type="button" data-modal-target="tipologias" data-modal-toggle="tipologias" wire:click="Edit({{$tipologia->id}})" class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded group bg-gradient-to-br from-blue-800 to-blue-800 group-hover:from-blue-200 group-hover:ring-blue-200 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-                                                    <span class="relative px-2 py-1 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded group-hover:bg-opacity-0 text-blue-800 hover:text-white" style="font-size: 15px">
-                                                        <i class="fa-solid fa-pen-to-square"></i>
-                                                    </span>
-                                                </button>
-                                                <button onclick="Comfirm('{{ $tipologia->id }}')" class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded group bg-gradient-to-br from-blue-400 to-blue-500 group-hover:from-blue-00 group-hover:ring-blue-300 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:red-blue-300 dark:focus:ring-blue-800">
-                                                    <span class="relative px-2 py-1 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded group-hover:bg-opacity-0 text-blue-800 hover:text-white" style="font-size: 15px">
-                                                        <i class="fa-solid fa-circle-xmark"></i>
-                                                    </span>
-                                                </button>
-                                            </div>
-                                        </td>
-                                    </tr>
+                                        <tr
+                                            class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                            <td class="px-6 py-4">{{ $tipologia->id }}</td>
+                                            <td class="px-6 py-4 text-center">{{ $tipologia->nombre_uni }}</td>
+                                            <td class="px-6 py-4 text-center">{{ $tipologia->abreviatura }}</td>
+                                            <td class="px-6 py-4 text-center">{{ $tipologia->estatus }}</td>
+                                            <td class="text-end">
+                                                <div class="btn-group flex  justify-end " role="group"
+                                                    aria-label="Basic example">
+                                                    <div class="flex   justify-around">
+                                                        <button type="button" data-modal-target="tipologias"
+                                                            data-modal-toggle="tipologias"
+                                                            wire:click="Edit({{ $tipologia->id }})"
+                                                            class="text-white bg-gradient-to-r from-blue-700 via-blue-700 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium   hover:text-white rounded text-sm px-5 py-2.5 text-center me-2 mb-2 w-7 h-9  flex items-center justify-center">
+                                                            <i class="fa-solid fa-pen-to-square text-center"></i>
+                                                        </button>
+
+                                                        <button type="button" data-modal-target="tipologias"
+                                                            data-modal-toggle="tipologias"
+                                                            onclick="Comfirm('{{ $tipologia->id }}')"
+                                                            class="text-white bg-gradient-to-r from-blue-500 via-blue-500 to-blue-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium   hover:text-white rounded text-sm px-5 py-2.5 text-center me-2 mb-2 w-7 h-9 flex items-center justify-center">
+                                                            <i class="fa-light fa-trash-can-xmark"></i>
+                                                        </button>
+                                                    </div>
+
+                                                </div>
+                                            </td>
+                                        </tr>
                                     @endforeach
                                 </tbody>
                             </table>
@@ -118,6 +137,13 @@
 
             let body = document.querySelector('body')
             body.removeChild(body.lastChild)
+            Swal.fire({
+                position: "top-end",
+                icon: "success",
+                title: "Your work has been saved",
+                showConfirmButton: false,
+                timer: 700
+            });
 
         });
         Livewire.on('category-updated', (event) => {
@@ -131,19 +157,16 @@
 
     });
 
-    function Comfirm(id) {
-        //     if(products > 0){
 
-        //     Swal.fire('NO SE PUEDE ELIMINAR LA CATEGORIA POR QUE TIENE PRODUCTOS RELACIONADOS')
-        //     return;
-        // }
+    function Comfirm(id) {
+
         Swal.fire({
             title: "Estas seguro de eliminar la tipologia?",
             icon: 'warning',
             showDenyButton: true,
             // showCancelButton: true,
-            confirmButtonText: "Save",
-            denyButtonText: `Don't save`,
+            confirmButtonText: "Guardar",
+            denyButtonText: `Cancelar`,
             customClass: {
                 popup: 'my-custom-alert',
                 title: 'my-custom-title',
