6d51c6006f6f4c02a1e81bdfafa77122d2ea6c82
gabriel <gaboandres1197@gmail.com>
2024-10-18 14:06:55 -0500

    calculo de egresos  18-10-2024 14:06 pm

--- resources/views/livewire/suppliers/supplier.blade.php
+++ resources/views/livewire/suppliers/supplier.blade.php
@@ -1,7 +1,6 @@
 <!-- resources/views/livewire/suppliers/supplier.blade.php -->
 <div>
 
-
     <h2 class="font-semibold text-xl text-gray-800 leading-tight text-center  pt-5" style="font-size: 34px;">
         {{ __('Listado de proveedores') }}
     </h2>
