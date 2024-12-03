ddea012d66c0485bea97a472d34b33189bed40a9
gabriel <gaboandres1197@gmail.com>
2024-12-03 09:59:28 -0500

    arreglo de migraciones y datos genericos en tipos de comercio y unidades de medida

--- database/migrations/2024_08_01_195630_create_commerce_types_table.php
+++ database/migrations/2024_08_01_195630_create_commerce_types_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 class CreateCommerceTypesTable extends Migration
 {
@@ -18,6 +19,12 @@ class CreateCommerceTypesTable extends Migration
             $table->string('name')->unique();
             $table->timestamps();
         });
+        // Insertar el registro genérico
+        DB::table('commerce_types')->insert([
+            'name' => 'Tienda de barrio',
+            'created_at' => now(), // Fecha y hora actuales
+            'updated_at' => now(),
+        ]);
     }
 
     /**
--- database/migrations/2024_08_16_192202_create_units_table.php
+++ database/migrations/2024_08_16_192202_create_units_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 class CreateUnitsTable extends Migration
 {
@@ -15,7 +16,41 @@ class CreateUnitsTable extends Migration
             $table->timestamps();
             $table->softDeletes(); // Para Soft Deletes
         });
+        // Insertar registros iniciales
+        DB::table('units')->insert([
+            [
+                'name' => 'Kilogramo',
+                'abbreviation' => 'kg',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Litro',
+                'abbreviation' => 'L',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Unidad',
+                'abbreviation' => 'u',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Metro',
+                'abbreviation' => 'm',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Caja',
+                'abbreviation' => 'caja',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
     }
+    
 
     public function down()
     {
